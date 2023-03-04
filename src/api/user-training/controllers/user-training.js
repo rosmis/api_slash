3d64fea00df60ea8a68df238aebce2a4930d8030
rosmis <rosmis123@gmail.com>
2023-03-04 14:48:28 +0100

    init

---
+++ src/api/user-training/controllers/user-training.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-training controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-training.user-training');
---
+++ src/api/user-training/routes/user-training.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-training router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-training.user-training');
