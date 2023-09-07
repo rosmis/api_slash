67319a9302005c64e6b8ab8945036ed469963996
rosmis <rosmis123@gmail.com>
2023-09-07 08:10:51 -0500

    init mailgun config

--- config/plugins.js
+++ config/plugins.js
@@ -1,27 +1,41 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    ezforms:{
-      config:{
-        captchaProvider: {
-          name: 'none',
-        },
-        notificationProviders: [],
-        enableFormName: true
-      }
-  }
-    // ...
-  });
+  },
+  ezforms: {
+    config: {
+      captchaProvider: {
+        name: "none",
+      },
+      notificationProviders: [],
+      enableFormName: true,
+    },
+  },
+  email: {
+    config: {
+      provider: "mailgun",
+      providerOptions: {
+        key: env("MAILGUN_API_KEY"), // Required
+        domain: env("MAILGUN_DOMAIN"), // Required
+        url: "https://api.eu.mailgun.net",
+      },
+      settings: {
+        defaultFrom: "myemail@protonmail.com",
+        defaultReplyTo: "myemail@protonmail.com",
+      },
+    },
+  },
+  // ...
+});
