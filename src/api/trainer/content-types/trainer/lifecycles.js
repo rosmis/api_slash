aa159a6f05afe7b40a53d85e2da809c0677f18b3
rosmis <rosmis123@gmail.com>
2023-09-07 11:13:24 -0500

    send email for tutor credit card info filling

--- src/api/trainer/content-types/trainer/lifecycles.js
+++ src/api/trainer/content-types/trainer/lifecycles.js
@@ -21,7 +21,7 @@ module.exports = {
 
       if (!authUser.user) return;
 
-      const { email: _userEmail, id: userId } = authUser.user;
+      const { email: userEmail, id: userId } = authUser.user;
 
       const { data: userInfos } = await supabase.from("profiles").select("*").eq("id", userId);
 
@@ -38,6 +38,15 @@ module.exports = {
         .eq("id", userId);
 
       // 2.send user email to invite him to fill his credit card infos
+
+      await strapi.plugins["email"].services.email.send({
+        to: userEmail,
+        from: "contact@slash-mentoring.com",
+        template_id: "d-234c43cfcada4f1f9dbb2593a1a0c2bc",
+        dynamicTemplateData: {
+          firstName: userInfos[0].firstName,
+        },
+      });
     } catch (error) {
       console.error(error);
     }
--- src/api/user-training/content-types/user-training/lifecycles.js
+++ src/api/user-training/content-types/user-training/lifecycles.js
@@ -1,35 +1,36 @@
-const { default: axios } = require("axios");
-
 module.exports = {
-    async afterCreate(event) {    // Connected to "Save" button in admin panel
-        const { result } = event;
+  async afterCreate(event) {
+    // Connected to "Save" button in admin panel
+    const { result } = event;
 
-        try{
-            console.log('result', result)
+    try {
+      console.log("result", result);
 
-            const training = await axios.get(
-                `http://localhost:1337/api/trainings/${+result.trainingId}`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
-                        "Content-Type": "application/json",
-                        },
-                }
-            )
+      //TODO fix this shit dunno why i used axios in backend
 
-            console.log('training', training.data.data.attributes.title)
+      // const training = await axios.get(
+      //     `http://localhost:1337/api/trainings/${+result.trainingId}`,
+      //     {
+      //         headers: {
+      //             Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
+      //             "Content-Type": "application/json",
+      //             },
+      //     }
+      // )
 
-            await strapi.plugins['email'].services.email.send({
-              to: 'maxence.lehee@skilder.fr',
-              from: 'rosmis123@gmail.com', // e.g. single sender verification in SendGrid
-              template_id: "d-26ece53af37d4eb2bc8caff454d28d9b",
-              dynamicTemplateData: {
-                  trainingTitle: training.data.data.attributes.title,
-                  trainingDuration: training.data.data.attributes.duration
-              }
-            })
-        } catch(err) {
-            console.log(err);
-        }
+      console.log("training", training.data.data.attributes.title);
+
+      await strapi.plugins["email"].services.email.send({
+        to: "maxence.lehee@skilder.fr",
+        from: "rosmis123@gmail.com", // e.g. single sender verification in SendGrid
+        template_id: "d-26ece53af37d4eb2bc8caff454d28d9b",
+        dynamicTemplateData: {
+          trainingTitle: training.data.data.attributes.title,
+          trainingDuration: training.data.data.attributes.duration,
+        },
+      });
+    } catch (err) {
+      console.log(err);
     }
-}
+  },
+};
